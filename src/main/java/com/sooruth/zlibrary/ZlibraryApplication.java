24a12888f797d7202e6b22bbfba9490ae5c678a7
zulfekar <17052721+ZS25@users.noreply.github.com>
2024-04-18 00:26:15 -0400

    First commit

---
+++ src/main/java/com/sooruth/zlibrary/ZlibraryApplication.java
@@ -0,0 +1,13 @@
+package com.sooruth.zlibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZlibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZlibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sooruth/zlibrary/ZlibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sooruth.zlibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZlibraryApplicationTests {
+
+/*	@Test
+	void contextLoads() {
+	}*/
+
+}
